0e49045960b508ce6b573bebb0116c95243edddc
huangtiande <htd0324@gmail.com>
2014-04-02 16:48:32 +0800

    增加一些测试case

--- src/test/java/com/laudandjolynn/paper2swf/Office2PdfTest.java
+++ src/test/java/com/laudandjolynn/paper2swf/Office2PdfTest.java
@@ -1,13 +1,11 @@
 package com.laudandjolynn.paper2swf;
 
-import junit.framework.TestCase;
-
 /**
  * @author: Laud
  * @email: htd0324@gmail.com
- * @date: 2014年4月2日 下午4:29:28
+ * @date: 2014年4月2日 下午4:47:59
  * @copyright: www.laudandjolynn.com
  */
-public class Paper2SwfTest extends TestCase {
+public class Office2PdfTest {
 
 }
--- src/test/java/com/laudandjolynn/paper2swf/Pdf2SwfTest.java
+++ src/test/java/com/laudandjolynn/paper2swf/Pdf2SwfTest.java
@@ -5,9 +5,9 @@ import junit.framework.TestCase;
 /**
  * @author: Laud
  * @email: htd0324@gmail.com
- * @date: 2014年4月2日 下午4:29:28
+ * @date: 2014年4月2日 下午4:48:08
  * @copyright: www.laudandjolynn.com
  */
-public class Paper2SwfTest extends TestCase {
+public class Pdf2SwfTest extends TestCase {
 
 }
