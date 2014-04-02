de1d091a6429118b3a7f81e611bbdeb3ead1e988
huangtiande <htd0324@gmail.com>
2014-04-02 16:30:00 +0800

    初始化

---
+++ src/main/java/com/laudandjolynn/paper2swf/Paper2Swf.java
@@ -0,0 +1,11 @@
+package com.laudandjolynn.paper2swf;
+
+/**
+ * @author: Laud
+ * @email: htd0324@gmail.com
+ * @date: 2014年4月2日 下午4:29:42
+ * @copyright: www.laudandjolynn.com
+ */
+public class Paper2Swf {
+
+}
