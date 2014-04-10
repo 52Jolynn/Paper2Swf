2e41c270aff790cb3d8e8a32b752ffdad9010339
huangtiande <htd0324@gmail.com>
2014-04-10 11:40:50 +0800

    更換开源协议header

--- src/main/java/com/laudandjolynn/paper2swf/Office2SwfConvertFunction.java
+++ src/main/java/com/laudandjolynn/paper2swf/Office2SwfConvertFunction.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2014 htd0324@gmail.com.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     htd0324@gmail.com - initial API and implementation
+ ******************************************************************************/
 package com.laudandjolynn.paper2swf;
 
 import org.gearman.client.GearmanJobResult;
